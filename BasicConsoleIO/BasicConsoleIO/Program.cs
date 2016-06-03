c8884de76f4ef9f57974c1b310158ab1792d9345
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-06-02 21:35:15 -0300

    BasicIO

---
+++ BasicConsoleIO/BasicConsoleIO/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConsoleIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Basic Console I/O ****");
+            GetUserData();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+           
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+
+        private static void GetUserData()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            //Get name and age
+            Console.Write("Please enter your name: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            string userName = Console.ReadLine();
+
+            Console.ForegroundColor = prevColor;
+            Console.Write("Please enter your age: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string userAge = Console.ReadLine();
+
+            //Change echo color
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            //Echo to the console
+            Console.WriteLine("Hello {0}! You are {1} years old!", userName, userAge);       
+        }
+    }
+}
